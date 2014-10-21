07c8f88e1ae8dbbc9c3cb542d562654b77661d7c
Timon <timonzimm@gmail.com>
2014-10-21 12:17:46 +0200

    cut the sticks and game of thrones 1

---
+++ Algorithms/Warmup/GameOfThrones1.scala
@@ -0,0 +1,10 @@
+object Solution {
+
+  def main(args: Array[String]) {
+    var line = readLine
+
+    println(
+      if (line.distinct.toCharArray.map(x => line.count(_ == x)).filter(_%2 != 0).size <= 1) "YES" else "NO"
+    )
+  }
+}
